f40fbf161a4b2abdac9019b397e6730368a8377d
Mikko Salovaara <hoofhb@gmail.com>
2020-12-13 23:46:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,28 +18,28 @@ void copy(const char* text)
 
 void CheckValidHardwareID()
 {
-	DWORD MY_HWID = -1372710529;
-	DWORD MY_HWID2 = 514760327;
-	DWORD MY_HWID3 = 1488013534;
-	DWORD MY_HWID4 = -895532923;
+	DWORD MY_HWID[5] = { -1372710529,514760327, 1488013534, 895532923, -91202064 };
 	DWORD dwSerial;
 	GetVolumeInformation("C:\\", NULL, 0, &dwSerial, NULL, NULL, NULL, 0);
-	if (dwSerial == MY_HWID || dwSerial == MY_HWID2 || dwSerial == MY_HWID3 || dwSerial == MY_HWID4)
-	{
-		MessageBox(0, "HWID Ok!", "Loaded", MB_OK | MB_ICONINFORMATION);
-		printf("Your HWID is: %d", dwSerial);
-	}
-	else
-	{
-		printf("Your HWID is: %d\n", dwSerial);
 
-		char buf[100];
-		sprintf_s(buf, 100, "%d", dwSerial);
-		copy(buf);
-
-		MessageBox(0, "Wrong HWID. HWID copied to clipboard!", "Error", MB_OK | MB_ICONERROR);
-		ExitProcess(1);
+	for (int i = 0; i < 5; i++)
+	{ 
+			if (dwSerial == MY_HWID[i])
+			{
+				MessageBox(0, "HWID Ok!", "Loaded", MB_OK | MB_ICONINFORMATION);
+				printf("Your HWID is: %d", dwSerial);
+				ExitProcess(1);
+			}	
 	}
+
+			printf("Your HWID is: %d\n", dwSerial);
+
+			char buf[100];
+			sprintf_s(buf, 100, "%d", dwSerial);
+			copy(buf);
+
+			MessageBox(0, "Wrong HWID. HWID copied to clipboard!", "Error", MB_OK | MB_ICONERROR);
+			ExitProcess(1);
 }
 
 int main()
